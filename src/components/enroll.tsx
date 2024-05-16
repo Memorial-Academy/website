87e2104430f7cefd0219afa64ba78a23eaf0c2e3
Griffin Ferguson <griffinferguson.us@gmail.com>
2024-05-16 12:32:24 -0500

    better enroll button link handling

--- src/components/enroll.tsx
+++ src/components/enroll.tsx
@@ -4,7 +4,7 @@ export default function Enroll({link, text}: {link: string, text: string}) {
     return (
         <div className="enroll-button-wrapper">
             <div className="enroll-button-container">
-                <a href={link} target="_blank" className="enroll-button">{text}</a>
+                <a href={link} target={link.startsWith("https://") ? "_blank" : "_parent"} className="enroll-button">{text}</a>
             </div>
         </div>
     )
